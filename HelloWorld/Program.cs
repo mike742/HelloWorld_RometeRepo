85595b0fa7e186d7d41e14a2d0665586ed993eb9
Mike Zorin <mike.zorin@robertsoncollege.ca>
2021-07-07 09:51:11 -0500

    added OOP

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,20 +2,64 @@
 
 namespace HelloWorld
 {
+    class Customer
+    {
+        private string _firstName = "N/A";
+        private string _lastName = "N/A";
+
+        private static string className;
+
+        public static string GetClassName() => className;
+        public static void SetClassName(string value) => className = value;
+
+
+        private double _salary;
+
+        public double Salary {
+            get => _salary;
+            set => _salary = value;
+        }
+
+        static Customer()
+        {
+            Console.WriteLine("Static cunstructor called");
+            className = "classCustomer";
+        }
+
+        public Customer(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+        public Customer() { }
+        // public Customer() : this(null, null) { }
+
+        public void PrintFullName()
+        {
+            Console.WriteLine($"{_firstName} {_lastName}");
+        }
+
+        public override string ToString() => $"{_firstName} {_lastName}";
+
+        public void DisplayName() => Console.WriteLine(ToString());
+        
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer c1 = new Customer();
+            c1.PrintFullName();
 
+            Console.WriteLine(Customer.GetClassName());
 
-            // new feature
-            int res = 5 * 4;
+            Customer.SetClassName("Customer class");
 
-            Console.WriteLine("res = " + res);
+            Console.WriteLine(Customer.GetClassName());
 
-            // feature 2
-            Console.WriteLine("Second feature here!");
+            c1.Salary = 1500.55;
+            Console.WriteLine("salary = ${0}", c1.Salary);
         }
     }
 }
